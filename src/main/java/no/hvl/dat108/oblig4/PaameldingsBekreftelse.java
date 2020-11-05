8a0193db27b4b1e186a591affdff7c563fdb4cce
NorWilhelm <seb.wilhe@gmail.com>
2020-11-05 19:36:09 +0100

    Lastet opp alle sidene
    Lastet opp JSP-sider med servlets

---
+++ src/main/java/no/hvl/dat108/oblig4/PaameldingsBekreftelse.java
@@ -0,0 +1,45 @@
+package no.hvl.dat108.oblig4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Innlogg
+ */
+@WebServlet(name ="PaameldingsBekreftelse", urlPatterns= "/paameldingsbekreftelse")
+public class PaameldingsBekreftelse extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+	/*
+	String password;
+	public void init() {
+		password = getServletConfig().getInitParameter("password");
+	}
+	 */
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // TODO: Feilmeldingsboks
+        // List<Melding> meldinger = meldingEAO.henteNSisteMeldinger(visningsantall);
+        // request.setAttribute("meldinger", meldinger);
+
+        request.getRequestDispatcher("WEB-INF/paameldingsbekreftelse.jsp").forward(request, response);
+    }
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: Behandle innloggingen
+        String passord = request.getParameter("passord");
+
+        response.sendRedirect("logginn?feilkode=1");
+
+        response.sendRedirect("Handleside");
+    }
+}
---
+++ target/TestProject-1.0-SNAPSHOT/WEB-INF/paameldingsbekreftelse.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sebastian
+  Date: 05.11.2020
+  Time: 14:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <!-- Fra https://purecss.io/ -->
+    <link rel="stylesheet"
+          href="https://unpkg.com/purecss@1.0.0/build/pure-min.css">
+    <title>Påmeldingsbekreftelse</title>
+</head>
+<body>
+<h2>Påmeldingsbekreftelse</h2>
+<p>Påmeldingen er mottatt for</p>
+<p>
+    &nbsp;&nbsp;&nbsp;Anne<br />
+    &nbsp;&nbsp;&nbsp;Annesen<br />
+    &nbsp;&nbsp;&nbsp;44554455<br /> &nbsp;&nbsp;&nbsp;kvinne
+</p>
+<a href="deltagerliste">Gå til deltagerlisten</a>
+</body>
+</html>
